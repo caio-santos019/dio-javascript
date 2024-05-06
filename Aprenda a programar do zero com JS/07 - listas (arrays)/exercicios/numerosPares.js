44c3fdbaa15e5902cf659ea073a178d732125a7b
caio-santos019 <caio.leme10@gmail.com>
2024-05-06 19:14:55 -0300

    arrays

---
+++ Aprenda a programar do zero com JS/07 - listas (arrays)/exercicios/numerosPares.js
@@ -0,0 +1,8 @@
+const numeros = [7, 12, 25, 36, 48, 85];
+
+for (let i = 0; i < numeros.length; i++) {
+  const numero = numeros[i];
+  if (numero % 2 === 0) {
+    console.log(numero);
+  }
+}
---
+++ Aprenda a programar do zero com JS/07 - listas (arrays)/exercicios/tabuada.js
@@ -0,0 +1,6 @@
+const numero = 7;
+
+for (let i = 1; i <= 10; i++) {
+  const tabuada = numero * i;
+  console.log(numero + " x " + i + " = " + tabuada);
+}
